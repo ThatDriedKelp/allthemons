d277d01f65b8f14175971cc26629a3d657cda89d
ThatDriedKelp <thestudbrains@gmail.com>
2026-06-04 12:20:41 -0400

    Update mons.js

--- mons.js
+++ mons.js
@@ -1,17 +1,17 @@
 function getRandomImageUrl() {
-  const width = 150;
-  const height = 150;
-  return `assets/imagesHQ/${width}/${height}?random=${Math.random()}`;
+  const width = 500;
+  const height = 500;
+  return `mons/${width}/${height}?random=${Math.random()}`;
 }
 
 function generateImages() {
   const imagesContainer = document.getElementById("images");
   imagesContainer.innerHTML = "";
-  for (let i = 0; i < 3; i++) {
+
     const img = document.createElement("img");
     img.src = getRandomImageUrl();
     imagesContainer.appendChild(img);
-  }
+
 }
 
 document
